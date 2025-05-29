c9c87cfbf492ff94f51f24af98399077badce785
Vinudithahub <168930150+Vinudithahub@users.noreply.github.com>
2025-05-29 17:39:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
      🤖 Multi-functional WhatsApp User Bot by Manul Official               
      🔗 GitHub   : https://github.com/manulofficial7                        
      ▶️ YouTube  : https://youtube.com/@ManulOfficialTech                   
-     📱 WhatsApp : +94 74 227 4855                                            
+     📱 WhatsApp : +94 719754197                                          
                                                                             
    ╭────────────────────────────────────────────────────────────────────╮
 //███╗░░░███╗░█████╗░███╗░░██╗██╗░░░██╗
@@ -41,6 +41,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Put_Your_Session_ID', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&mBNCjBCB#cWVS7R-ujMAzeZDLVimLozXO2zSmDKUBnVTyRNLTpFY', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000'
 };
